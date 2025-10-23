dd22a1edaa66e744986f94a8bd382c4e6d79edba
Диана Никулина <djigurdiana@mail.ru>
2025-10-23 15:04:04 +0500

    задание 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("А.С. Пушкин");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Узник");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Сижу за решеткой в темнице сырой.");
+            Console.WriteLine("Вскормленный в неволе орел молодой.");
+            Console.WriteLine("Мой грустный товарищ, махая крылом,");
+            Console.WriteLine("Кровавую пищу клюет под окном.");
+
+        }
+    }
+}
